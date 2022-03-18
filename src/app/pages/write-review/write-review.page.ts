2109d524c51077b8cd5d4f5389c8d638859b5e8b
Juan Urdaneta <67289885+Juandurdaneta@users.noreply.github.com>
2022-03-18 16:30:26 -0400

    Added logic to post reviews

--- src/app/pages/write-review/write-review.page.ts
+++ src/app/pages/write-review/write-review.page.ts
@@ -4,6 +4,8 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
 import { ReviewService } from 'src/app/services/review.service';
 import { UserService } from 'src/app/services/user.service';
+import { ToastController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-write-review',
@@ -21,7 +23,8 @@ export class WriteReviewPage implements OnInit {
     private authService: AuthService,
     private router: Router, 
     private formBuilder: FormBuilder,
-    private userService: UserService
+    private userService: UserService,
+    private toastController : ToastController
 
   ) { }
 
@@ -38,15 +41,37 @@ export class WriteReviewPage implements OnInit {
 
     this.reviewForm = this.formBuilder.group({
       comment: [null, [Validators.required]],
-      calification: [null, [Validators.min(1), Validators.max(10)]]
+      rating: [null, [Validators.min(1), Validators.max(10)]]
     })
 
   }
 
 
 
-  publish(movieId, userId){
-    console.log(movieId, userId)
+  async publish(movieId, userId){
+    console.log("loading...")
+    let myForm: FormData = new FormData();
+    myForm.append("movieId", movieId)
+    myForm.append("userId", userId)
+    myForm.append("comment", this.reviewForm.get('comment').value)
+    myForm.append("rating", this.reviewForm.get('rating').value)
+  
+    this.reviewService.postReview(myForm)
+    .subscribe(
+      async(res)=>{
+
+        this.presentToast(res.message, res.status)
+
+
+        if(res.status == 200){
+
+          this.reviewForm.controls['comment'].setValue('');
+          this.reviewForm.controls['rating'].setValue('');
+
+
+        }
+      }
+    )
   }
 
   getUserId(){
@@ -57,6 +82,16 @@ export class WriteReviewPage implements OnInit {
       }
     )
   }
+
+  async presentToast(message, status) {
+    const toast = await this.toastController.create({
+      message: message,
+      duration: 2000,
+      color: status == 200 ? "success" : "danger"
+    });
+    toast.present();
+  }
+
 
 
 }
