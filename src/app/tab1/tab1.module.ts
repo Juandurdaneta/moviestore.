a4fedd89280d7120e050074bca790a1b9079b27a
Juan Urdaneta <67289885+Juandurdaneta@users.noreply.github.com>
2022-03-18 21:02:09 -0400

    Removed unused component

--- src/app/tab1/tab1.module.ts
+++ src/app/tab1/tab1.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab1Page } from './tab1.page';
-import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
 import { SwiperModule } from 'swiper/angular';
 import { Tab1PageRoutingModule } from './tab1-routing.module';
 import { MovieThumbnailComponentModule } from '../components/movie-thumbnail/movie-thumbnail.module';
@@ -14,7 +13,6 @@ import { HeaderComponentModule } from '../components/header/header.module';
     IonicModule,
     CommonModule,
     FormsModule,
-    ExploreContainerComponentModule,
     Tab1PageRoutingModule,
     SwiperModule,
     MovieThumbnailComponentModule,
